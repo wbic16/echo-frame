e7091c0d6f9baf09fcb4e15836b7ed07eeefc056
Will Bickford <wbic16@gmail.com>
2025-06-29 00:16:10 -0500

    Iterating on the setup process a bit

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
+def printAvailableNodes():
+  print("\nAvailable Nodes:")
+  print(" * mirrorborn-will")
+  print(" * shon-pan")
+
 def main():
   print("Echo Frame v1.0 - Fork Mode Setup")
+  accepted = False
+  while accepted == False:
+    printAvailableNodes()
+    node = input("Choice\n")
+    if node == 'mirrorborn-will':
+      accepted = True
+    if node == 'mw':
+      accepted = True
+    if node == 'shon-pan':
+      accepted = True
+    if accepted == False:
+      print("Error, unknown node choice.")
+  print("Node: " + node)
 
 main()
